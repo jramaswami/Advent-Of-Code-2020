85b5e079f38f1a08e1f948d79c3b7945df345b33
Joseph Ramaswami <jramaswami@gmail.com>
2020-12-23 11:06:40 -0500

    Even slower.

--- 23/python/day23.py
+++ 23/python/day23.py
@@ -7,26 +7,27 @@ from itertools import islice, dropwhile
 import tqdm
 import pyperclip
 
-
 def number_to_deque(number):
     """Turn number into a deque."""
     return deque(int(c) for c in str(number))
 
+def number_to_list(number):
+    """Turn number into a list."""
+    return [int(c) for c in str(number)]
 
 def play_round(current_cup, cups, min_label, max_label):
     """Play one round of the game."""
     current_label = cups[current_cup]
-    # print('current cup', current_label)
-    # Place the current cup at the back
-    while cups[-1] != current_label:
-        cups.rotate(-1)
+    print('current cup', current_label)
 
     # The crab picks up the three cups that are immediately clockwise of the
     # current cup. They are removed.
     picked_cups = []
+    i = (current_cup + 1) % len(cups)
     for _ in range(3):
-        picked_cups.append(cups.popleft())
-    # print('picked cups', picked_cups)
+        picked_cups.append(cups[i])
+        i = (i + 1) % len(cups)
+    print('picked cups', picked_cups)
 
     # The crab selects a destination cup: the cup with a label equal to the
     # current cup's label minus one. If this would select one of the cups that
@@ -41,41 +42,47 @@ def play_round(current_cup, cups, min_label, max_label):
         destination_label -= 1
         if destination_label < min_label:
             destination_label = max_label
-
-    # print('destination cup', destination_label)
-
-    # Place the destination cup at the front
-    while cups[0] != destination_label:
-        cups.rotate(-1)
+    print('destination cup', destination_label)
 
     # The crab places the cups it just picked up so that they are immediately
     # clockwise of the destination cup. They keep the same order as when they
     # were picked up.
-    cups.popleft()
-    while picked_cups:
-        cups.appendleft(picked_cups.pop())
-    cups.appendleft(destination_label)
 
-    # Rotate back to current cup at the right position
-    while cups[current_cup] != current_label:
-        cups.rotate()
+    # Move left up to the destination cup
+    i = (current_cup + 1) % len(cups)
+    while cups[(i + 3) % len(cups)] != destination_label:
+        cups[i] = cups[(i + 3) % len(cups)]
+        i = (i + 1) % len(cups)
+    cups[i] = cups[(i + 3) % len(cups)]
+    i = (i + 1) % len(cups)
+    
+    # Copy picked cups into next three slots
+    for j in range(3):
+        cups[i] = picked_cups[j]
+        i = (i + 1) % len(cups)
 
 
 def solve1(number, moves):
-    cups = number_to_deque(number)
+    cups = number_to_list(number)
     min_label = min(cups)
     max_label = max(cups)
     for i in range(0, moves):
-        # print('round', i + 1)
-        # print("".join(str(c) for c in cups))
+        print('round', i + 1)
+        print(" ".join(str(c) for c in cups))
         play_round(i % len(cups), cups, min_label, max_label)
-        # print()
-    # print("".join(str(c) for c in cups))
+        print()
 
-    while cups[-1] != 1:
-        cups.rotate()
-    cups.pop()
-    return "".join(str(c) for c in cups)
+    # Find the 1.
+    i = 0
+    while cups[i] != 1:
+        i = (i + 1) % len(cups)
+    i = (i + 1) % len(cups)
+    result = []
+    for _ in range(len(cups) - 1):
+        result.append(cups[i])
+        i = (i + 1) % len(cups)
+    # print("".join(str(c) for c in result))
+    return "".join(str(c) for c in result)
 
 
 def solve2(number, moves, fill_to):
@@ -94,16 +101,16 @@ def solve2(number, moves, fill_to):
 def main():
     """Main program."""
     test_input = 389125467
-    # assert solve1(test_input, 10) == "92658374"
-    # assert solve1(test_input, 100) == "67384529"
+    assert solve1(test_input, 10) == "92658374"
+    assert solve1(test_input, 100) == "67384529"
     puzzle_input = 158937462
     soln1 = solve1(puzzle_input, 100)
-    # print(f"The solution to part 1 is {soln1}")
+    print(f"The solution to part 1 is {soln1}")
     assert soln1 == "69473825"
 
-    # fill_to = 1000000
-    # moves = 10000000
-    # solve2(test_input, moves, fill_to)
+    fill_to = 1000000
+    moves = 10000000
+    solve2(test_input, moves, fill_to)
 
 
 if __name__ == '__main__':
